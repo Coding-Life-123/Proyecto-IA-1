10bd6143ef282ec3b606cc1488f8aa581b74e760
Coding-Life-123 <dfordonezpina@gmail.com>
2025-09-22 21:41:12 -0500

    Santiago no, no me la chupes, AHHHHHHHHHHHHHHH

--- Backend/index.js
+++ Backend/index.js
@@ -1,1 +1,21 @@
-require('dotenv').config();
+import dotenv from 'dotenv';
+dotenv.config()
+
+
+const apiKey = process.env.API_KEY;
+
+console.log(apiKey)
+
+import { GoogleGenAI } from "@google/genai";
+
+const ai = new GoogleGenAI({});
+
+async function main() {
+  const response = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: "Explain how AI works in a few words",
+  });
+  console.log(response.text);
+}
+
+await main();
